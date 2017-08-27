3963ee059e2add1dd5bb56a51ae730f537799272
diegodifreitas <diegodifreitas@gmail.com>
2017-08-27 01:16:26 -0300

    Implemented user controller

--- server/modules/User/controller.ts
+++ server/modules/User/controller.ts
@@ -1,37 +1,54 @@
 import { Request, Response } from 'express';
 import * as HTTPStatus from 'http-status';
+import * as _ from 'lodash';
+
+import { onError } from '../../api/responses/errorHandler';
+import { onSucess } from '../../api/responses/sucessHandler';
+import { dbErrorHandler } from '../../config/dbErrorHandler';
+
+import User from './service';
 
 class UserController {
-  constructor() { }
+
+  public UserService: User;
+
+  constructor() {
+    this.UserService = new User();
+  }
 
   public getAll(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    this.UserService.getAll()
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao buscar todos os usuários'));
   }
 
   public createUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    this.UserService.create(req.body)
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(dbErrorHandler, res))
+      .catch(_.partial(onError, res, 'Erro ao inserir novo usuário'));
   }
 
   public getById(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    this.UserService.getById(userId)
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Usuario não encontrado'));
   }
 
   public updateUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    const props = req.body;
+    this.UserService.update(userId, props)
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao atualizar usuário'));
   }
 
   public deleteUser(req: Request, res: Response) {
-    res.status(HTTPStatus.OK).json({
-      message: 'OK',
-    });
+    const userId = parseInt(req.params.id, 10);
+    this.UserService.delete(userId)
+      .then(_.partial(onSucess, res))
+      .catch(_.partial(onError, res, 'Erro ao excluir usuário'));
   }
 }
 
