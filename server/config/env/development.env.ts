55dc15de0c05936ba7fd1f312331ab8a19f83c20
diegodifreitas <diegodifreitas@gmail.com>
2017-08-27 01:15:14 -0300

    Added new env configuration

--- server/config/env/development.env.ts
+++ server/config/env/development.env.ts
@@ -1,13 +1,13 @@
 /* tslint:disable */
 module.exports = {
   env: 'development',
-  db: 'moment',
+  db: 'doctor-prescription',
   dialect: 'postgres',
   username: 'postgres',
   password: 'postgres',
   host: 'localhost',
   serverPort: 3001,
   pgPort: 5432,
-  dbURL: 'postgres://postgres:postgres@localhost:5432/moment',
+  dbURL: 'postgres://postgres:postgres@localhost:5432/doctor-prescription',
   secret: 'S3cr3t',
 };
--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -1,13 +1,13 @@
 /* tslint:disable */
 module.exports = {
   env: 'development',
-  db: 'moment-test',
+  db: 'doctor-prescription-test',
   dialect: 'postgres',
   username: 'postgres',
   password: 'postgres',
   host: 'localhost',
   serverPort: 3001,
   pgPort: 5432,
-  dbURL: 'postgres://postgres:postgres@localhost:5432/moment-test',
+  dbURL: 'postgres://postgres:postgres@localhost:5432/doctor-prescription-test',
   secret: 'S3cr3t',
 };
