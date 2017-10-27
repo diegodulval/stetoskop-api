238c8834331c559ff26d80e271a082e502919495
diegodulval <diegodifreitas@gmail.com>
2017-10-27 01:59:48 -0200

    Remove dist folder

--- dist/server/config/env/config.js
+++
@@ -1,5 +0,0 @@
-var extension = 'js';
-if (process.env.NODE_ENV == 'development') {
-    extension = 'ts';
-}
-module.exports = function () { return require("../env/" + process.env.NODE_ENV + ".env." + extension); };
