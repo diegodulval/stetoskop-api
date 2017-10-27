c2f97a2aa2b6b1e8c34b639ce87040a15869aba2
diegodulval <diegodifreitas@gmail.com>
2017-10-27 02:01:35 -0200

    Clean repository for new project in JAVA language

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run heroku
+web: java -Dserver.port=$PORT -Dspring.profiles.active=prod $JAVA_OPTS -jar target/cursomc-0.0.1-SNAPSHOT.jar
--- server/config/env/config.ts
+++
@@ -1,5 +0,0 @@
-let extension: string = 'js';
-if (process.env.NODE_ENV == 'development') {
-  extension = 'ts';
-}
-module.exports = () => require(`../env/${process.env.NODE_ENV}.env.${extension}`);
