5035a21caf598f0ff26be156978f56e71d42c910
diegodulval <diegodifreitas@gmail.com>
2017-11-07 01:51:13 -0200

    Update User domain

--- src/main/java/com/dulval/stetoskop/domain/Address.java
+++ src/main/java/com/dulval/stetoskop/domain/Address.java
@@ -1,6 +1,5 @@
 package com.dulval.stetoskop.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
@@ -10,9 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+import lombok.Data;
 
+@Data
 @Entity
 public class Address implements Serializable {
 
@@ -24,77 +23,13 @@ public class Address implements Serializable {
     private String number;
     private String neighborhood;
     private String cep;
-
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    @MapsId
-    @JsonIgnore
-    private User user;
+    private String street;
 
     @ManyToOne
     @JoinColumn(name = "city_id")
     private City city;
 
     public Address() {
-    }
-
-    public Address(Integer id, String numero, String bairro, String cep,
-            User cliente, City cidade) {
-        super();
-        this.id = id;
-        this.number = numero;
-        this.neighborhood = bairro;
-        this.cep = cep;
-        this.user = cliente;
-        this.setCity(cidade);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getNeighborhood() {
-        return neighborhood;
-    }
-
-    public void setNeighborhood(String neighborhood) {
-        this.neighborhood = neighborhood;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public City getCity() {
-        return city;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
     }
 
 }
--- src/main/java/com/dulval/stetoskop/domain/User.java
+++ src/main/java/com/dulval/stetoskop/domain/User.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -13,14 +12,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
 @Entity(name = "sys_user")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User implements Serializable {
@@ -31,34 +31,30 @@ public abstract class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-
     @Column(unique = true)
     private String email;
-
     //@JsonIgnore
     private String password;
-
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+    @ManyToOne
+    @JoinColumn(name = "address_id")
     private Address address;
-
     private String phone;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "ROLES")
     private Set<Integer> roles = new HashSet<>();
 
-    public User(Integer id, String nome, String email, String senha, String phone) {
-        super();
-        this.id = id;
-        this.name = nome;
-        this.email = email;
-        this.password = senha;
-        this.phone = phone;
+    public User() {
         addRoles(Role.CLIENTE);
     }
 
-    public User() {
-        addRoles(Role.CLIENTE);
+    public User(Integer id, String name, String email, String password, Address address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
     }
 
     public Set<Role> getRoles() {
@@ -68,94 +64,4 @@ public abstract class User implements Serializable {
     public void addRoles(Role perfil) {
         roles.add(perfil.getCod());
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 89 * hash + Objects.hashCode(this.id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.email, other.email)) {
-            return false;
-        }
-        if (!Objects.equals(this.password, other.password)) {
-            return false;
-        }
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" + "id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", address=" + address + ", roles=" + roles + '}';
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    
 }
