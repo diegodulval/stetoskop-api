053973ebc7256b2eb34cefe47825cbf00dcfb0be
diegodulval <diegodifreitas@gmail.com>
2017-11-07 01:54:10 -0200

    Enable access for all resource api

--- src/main/java/com/dulval/stetoskop/config/SecurityConfig.java
+++ src/main/java/com/dulval/stetoskop/config/SecurityConfig.java
@@ -39,9 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS = {
         "/h2-console/**",
-        "/users/**",
-        "/doctors/**",
-        "/institutions/**"
+        "/**"
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {};
