b480c289cc3815c4845cd3b62b2005bc4483a5b9
diegodulval <diegodifreitas@gmail.com>
2017-11-07 11:53:01 -0200

    Added services

---
+++ src/main/java/com/dulval/stetoskop/services/MedicamentService.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dulval.stetoskop.services;
+
+import com.dulval.stetoskop.domain.InterationMedicament;
+import com.dulval.stetoskop.domain.Medicament;
+import com.dulval.stetoskop.repositories.InterationMedicamentRepository;
+import com.dulval.stetoskop.repositories.MedicamentRepository;
+import com.dulval.stetoskop.services.exceptions.DataIntegrityException;
+import com.dulval.stetoskop.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Diego Dulval
+ */
+@Service
+public class MedicamentService {
+
+    @Autowired
+    private MedicamentRepository repo;
+
+    @Autowired
+    private InterationMedicamentRepository iterationRepository;
+
+    public Medicament create(Medicament obj) {
+        obj.setId(null);
+        obj = repo.save(obj);
+
+        for (InterationMedicament interation : obj.getInterations()) {
+            interation.setMedicamentInteration(repo.findOne(interation.getMedicament().getId()));
+            interation.setMedicament(obj);
+        }
+        iterationRepository.save(obj.getInterations());
+        return obj;
+    }
+
+    public Medicament readById(Integer id) {
+        Medicament obj = repo.findOne(id);
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+                    + ", Tipo: " + Medicament.class.getSimpleName());
+        }
+        return obj;
+    }
+
+    public void delete(Integer id) {
+        readById(id);
+        try {
+            repo.delete(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há registros relacionados");
+        }
+    }
+
+    public Page<Medicament> read(String nameDecoded, Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Medicament update(Medicament obj) {
+        Medicament newObj = readById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(Medicament newObj, Medicament obj) {
+        newObj.setName(obj.getName());
+        newObj.setApresentations(obj.getApresentations());
+        newObj.setComercialNames(obj.getComercialNames());
+        newObj.setInterations(obj.getInterations());
+    }
+}
