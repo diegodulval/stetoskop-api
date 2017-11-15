d2a2c013cdfcc020181bf46b6f9f8cc712096841
diegodulval <diegodifreitas@gmail.com>
2017-11-15 19:01:11 -0200

    Added resource of the Unity

---
+++ src/main/java/com/dulval/stetoskop/resources/UnityResource.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dulval.stetoskop.resources;
+
+import com.dulval.stetoskop.domain.Unity;
+import com.dulval.stetoskop.services.UnityService;
+import java.net.URI;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+/**
+ *
+ * @author Diego Dulval
+ */
+@RestController
+@RequestMapping(value = "/unity")
+public class UnityResource {
+
+    @Autowired
+    private UnityService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity find(@PathVariable Integer id) {
+        Unity obj = service.readById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity insert(@Valid @RequestBody Unity obj) {
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@Valid @RequestBody Unity obj, @PathVariable Integer id) {
+        obj.setId(id);
+        service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity read(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        Page<Unity> list = service.read(page, linesPerPage, orderBy, direction);
+        return ResponseEntity.ok().body(list);
+    }
+}
