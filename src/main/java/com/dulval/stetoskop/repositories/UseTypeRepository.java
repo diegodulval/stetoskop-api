43d7959870f4a3510c3c9505bd00068e17a8a2b1
diegodulval <diegodifreitas@gmail.com>
2017-11-15 18:47:08 -0200

    Added repo of Unity and UseType

--- src/main/java/com/dulval/stetoskop/repositories/UseTypeRepository.java
+++ src/main/java/com/dulval/stetoskop/repositories/UseTypeRepository.java
@@ -1,11 +1,11 @@
 package com.dulval.stetoskop.repositories;
 
-import com.dulval.stetoskop.domain.Unity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dulval.stetoskop.domain.UseType;
 
 @Repository
-public interface UnityRepository extends JpaRepository<Unity, Integer> {
+public interface UseTypeRepository extends JpaRepository<UseType, Integer> {
 
 }
