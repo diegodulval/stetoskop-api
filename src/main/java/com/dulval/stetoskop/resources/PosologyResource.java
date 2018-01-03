120060eca6ee85b12a6049c65a498b525c6a220c
diegodulval <diegodifreitas@gmail.com>
2018-01-03 19:55:28 -0200

    Create new resource for Posology

--- src/main/java/com/dulval/stetoskop/resources/PosologyResource.java
+++ src/main/java/com/dulval/stetoskop/resources/PosologyResource.java
@@ -5,9 +5,9 @@
  */
 package com.dulval.stetoskop.resources;
 
-import com.dulval.stetoskop.domain.Pacient;
+import com.dulval.stetoskop.domain.Posology;
 import com.dulval.stetoskop.resources.utils.URL;
-import com.dulval.stetoskop.services.PacientService;
+import com.dulval.stetoskop.services.PosologyService;
 import java.net.URI;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,20 +29,20 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  * @author Diego Dulval
  */
 @RestController
-@RequestMapping(value = "/pacients")
-public class PacientResource {
+@RequestMapping(value = "/posology")
+public class PosologyResource {
 
     @Autowired
-    private PacientService service;
+    private PosologyService service;
 
     @GetMapping("/{id}")
     public ResponseEntity find(@PathVariable Integer id) {
-        Pacient obj = service.readById(id);
+        Posology obj = service.readById(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @PostMapping
-    public ResponseEntity insert(@Valid @RequestBody Pacient obj) {
+    public ResponseEntity insert(@Valid @RequestBody Posology obj) {
         obj = service.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -50,7 +50,7 @@ public class PacientResource {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@Valid @RequestBody Pacient obj, @PathVariable Integer id) {
+    public ResponseEntity update(@Valid @RequestBody Posology obj, @PathVariable Integer id) {
         obj.setId(id);
         service.update(obj);
         return ResponseEntity.noContent().build();
@@ -64,14 +64,14 @@ public class PacientResource {
 
     @GetMapping
     public ResponseEntity read(
-            @RequestParam(value = "name", defaultValue = "", required = false) String name,
+            @RequestParam(value = "description", defaultValue = "", required = false) String description,
             @RequestParam(value = "doctor", defaultValue = "0", required = false) Integer doctor,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
             @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
-        String nameDecoded = URL.decodeParam(name);
-        Page<Pacient> list = service.readByCriteria(nameDecoded, doctor, page, linesPerPage, orderBy, direction);
+        String descDecode = URL.decodeParam(description);
+        Page<Posology> list = service.readByCriteria(descDecode, doctor, page, linesPerPage, orderBy, direction);
         return ResponseEntity.ok().body(list);
     }
 }
