97fda04a6d1607240ce90e975dd2e9d11c4ccb60
diegodulval <diegodifreitas@gmail.com>
2018-01-04 16:05:18 -0200

    Create resource for State entity

---
+++ src/main/java/com/dulval/stetoskop/services/StateService.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dulval.stetoskop.services;
+
+import com.dulval.stetoskop.domain.State;
+import com.dulval.stetoskop.repositories.StateRepository;
+import com.dulval.stetoskop.services.exceptions.DataIntegrityException;
+import com.dulval.stetoskop.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Diego Dulval
+ */
+@Service
+public class StateService {
+
+    @Autowired
+    private StateRepository repo;
+
+    public State create(State obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
+
+    public State readById(Integer id) {
+        State obj = repo.findOne(id);
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+                    + ", Tipo: " + State.class.getSimpleName());
+        }
+        return obj;
+    }
+
+    public void delete(Integer id) {
+        readById(id);
+        try {
+            repo.delete(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há registros relacionados");
+        }
+    }
+
+    public Page<State> read(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public State update(State obj) {
+        State newObj = readById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(State newObj, State obj) {
+        newObj.setName(obj.getName());
+        newObj.setUf(obj.getUf());
+    }
+}
